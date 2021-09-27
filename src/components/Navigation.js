789131cc16409289db1457965d64d06ede22de9a
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-09-27 16:37:08 -0700

    removed files not needed, added first few edits to titles

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Navigation () {
+  return (
+    <div className="navigation">
+      <nav className="site-navigation">
+        <span>My React Blog</span>
+        <ul>
+          <li>DSA</li>
+          <li>Resources</li>
+          <li>Thoughts</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navigation;
